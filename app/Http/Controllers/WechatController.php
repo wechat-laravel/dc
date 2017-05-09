f955808b7e6cf17a94cd876c19bf7e49e8475700
hackqy <hackqy@qq.com>
2017-05-09 15:18:31 +0800

    overture

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -11,7 +11,7 @@ class WechatController extends Controller
      *
      * @return string
      */
-    
+
     public function serve()
     {
 
@@ -19,7 +19,33 @@ class WechatController extends Controller
 
         $wechat->server->setMessageHandler(function($message){
 
-            return "欢迎关注 overtrue！";
+            switch ($message->MsgType) {
+                case 'event':
+                    return '收到事件消息';
+                    break;
+                case 'text':
+                    return '收到文字消息';
+                    break;
+                case 'image':
+                    return '收到图片消息';
+                    break;
+                case 'voice':
+                    return '收到语音消息';
+                    break;
+                case 'video':
+                    return '收到视频消息';
+                    break;
+                case 'location':
+                    return '收到坐标消息';
+                    break;
+                case 'link':
+                    return '收到链接消息';
+                    break;
+                // ... 其它消息
+                default:
+                    return '收到其它消息';
+                    break;
+            }
 
         });
 
