a3e00f911361f6958a0b175fd275d3fd786b2109
hackqy <hackqy@qq.com>
2017-05-15 11:02:54 +0800

    菜单

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -60,36 +60,47 @@ class WechatController extends Controller
 
     public function addMenu()
     {
-        $buttons = [
-            [
-                "type" => "click",
-                "name" => "今日歌曲",
-                "key"  => "V1001_TODAY_MUSIC"
-            ],
-            [
-                "name"       => "菜单",
-                "sub_button" => [
-                    [
-                        "type" => "view",
-                        "name" => "搜索",
-                        "url"  => "http://www.soso.com/"
-                    ],
-                    [
-                        "type" => "view",
-                        "name" => "视频",
-                        "url"  => "http://v.qq.com/"
-                    ],
-                    [
-                        "type" => "click",
-                        "name" => "赞一下我们",
-                        "key" => "V1001_GOOD"
+        try{
+
+            $buttons = [
+                [
+                    "type" => "click",
+                    "name" => "今日歌曲",
+                    "key"  => "V1001_TODAY_MUSIC"
+                ],
+                [
+                    "name"       => "菜单",
+                    "sub_button" => [
+                        [
+                            "type" => "view",
+                            "name" => "搜索",
+                            "url"  => "http://www.soso.com/"
+                        ],
+                        [
+                            "type" => "view",
+                            "name" => "视频",
+                            "url"  => "http://v.qq.com/"
+                        ],
+                        [
+                            "type" => "click",
+                            "name" => "赞一下我们",
+                            "key" => "V1001_GOOD"
+                        ],
                     ],
                 ],
-            ],
-        ];
-        $menu = $this->wechat->menu;
+            ];
 
-        $menu->add($buttons);
+            $menu = $this->wechat->menu;
+
+            $menu->add($buttons);
+
+        }catch (\Exception $e){
+
+            return response()->json(['success'=>false,'msg'=>$e->getMessage()]);
+
+        }
+
+        return response()->json(['success'=>true,'msg'=>'创建成功']);
 
     }
 
