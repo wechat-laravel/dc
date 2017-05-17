64baa3a4f7b9e00523a8b77cf2d5b79d93f4539d
hackqy <hackqy@qq.com>
2017-05-17 15:03:10 +0800

    test

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -61,7 +61,7 @@
 <script type="text/javascript" charset="UTF-8">
     wx.config(<?=$js->config(['getLocation', 'onMenuShareTimeline','onMenuShareAppMessage'],false);?>);
     wx.ready(function(){
- 	wx.checkJsApi({
+        wx.checkJsApi({
             jsApiList: [
                 'getLocation',
                 'onMenuShareTimeline',
@@ -69,36 +69,55 @@
             ],
             success: function (res) {
                 if(res.checkResult.getLocation !== true){
-		    alert('当前设备不支持获取地理位置');
-		}
+                   alert('当前设备不支持获取地理位置');
+                }
                 if(res.checkResult.onMenuShareTimeline !== true){
-		    alert('当前设备不支持分享到朋友圈');
-		}
+                    alert('当前设备不支持分享到朋友圈');
+                }
                 if(res.checkResult.onMenuShareAppMessage !== true){
-		    alert('当前设备不支持分享给好友');
-		}
-            },
+                    alert('当前设备不支持分享给好友');
+                }
+            }
         });
-	wx.onMenuShareAppMessage({
-            title   : '测试哦',
-            desc    : '描述是个什么鬼',
-            link    : 'http://dc.leqiyi.cn/wechat/test/aaa',
-            imgUrl  : 'http://dc.leqiyi.cn/assets/images/user.jpg',
-	    type    : '',
-	    dataUrl : '',
-            trigger : function (res) {
-              alert('用户点击发送给朋友');
+//	    wx.onMenuShareAppMessage({
+//            title   : '测试哦',
+//            desc    : '描述是个什么鬼',
+//            link    : 'http://dc.leqiyi.cn/wechat/test/aaa',
+//            imgUrl  : 'http://demo.open.weixin.qq.com/jssdk/images/p2166127561.jpg',
+//	        type    : '',
+//	        dataUrl : '',
+//            trigger : function (res) {
+//              alert('用户点击发送给朋友');
+//            },
+//            success : function (res) {
+//               alert('已分享');
+//            },
+//            cancel  : function (res) {
+//               alert('已取消');
+//            },
+//            fail: function (res) {
+//               alert(JSON.stringify(res));
+//            }
+//        });
+        wx.onMenuShareAppMessage({
+            title: '互联网之子',
+            desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事，那些所谓本来如此，注定如此的事，它们其实没有非得如此，事情是可以改变的。更重要的是，有些事既然错了，那就该做出改变。',
+            link: 'http://movie.douban.com/subject/25785114/',
+            imgUrl: 'http://demo.open.weixin.qq.com/jssdk/images/p2166127561.jpg',
+            trigger: function (res) {
+                // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
+                alert('用户点击发送给朋友');
             },
-            success : function (res) {
-               alert('已分享');
+            success: function (res) {
+                alert('已分享');
             },
-            cancel  : function (res) {
-               alert('已取消');
+            cancel: function (res) {
+                alert('已取消');
             },
             fail: function (res) {
-               alert(JSON.stringify(res));
+                alert(JSON.stringify(res));
             }
-        });   
+        });
 
     });
 	
