a38d9b31cac14833f19e01f0441312956290359c
hackqy <hackqy@qq.com>
2017-06-14 19:06:40 +0800

    记录上级

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -6,59 +6,55 @@
     <meta name="description"content="dc的描述哦"> 
     <script src="{{ URL::asset('assets/js/jquery.min.js') }}" type="text/javascript" charset="utf-8"></script>
     <script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js" type="text/javascript" charset="utf-8"></script>
-    <style>
-        html, body {
-            height: 100%;
-        }
+    {{--<style>--}}
+        {{--html, body {--}}
+            {{--height: 100%;--}}
+        {{--}--}}
 
-        body {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            display: table;
-            font-weight: 100;
-            font-family: 'Lato';
-        }
+        {{--body {--}}
+            {{--margin: 0;--}}
+            {{--padding: 0;--}}
+            {{--width: 100%;--}}
+            {{--display: table;--}}
+            {{--font-weight: 100;--}}
+            {{--font-family: 'Lato';--}}
+        {{--}--}}
 
-        .container {
-            text-align: center;
-            display: table-cell;
-            vertical-align: middle;
-        }
+        {{--.container {--}}
+            {{--text-align: center;--}}
+            {{--display: table-cell;--}}
+            {{--vertical-align: middle;--}}
+        {{--}--}}
 
-        .content {
-            text-align: center;
-            display: inline-block;
-        }
+        {{--.content {--}}
+            {{--text-align: center;--}}
+            {{--display: inline-block;--}}
+        {{--}--}}
 
-        .title {
-            font-size: 96px;
-        }
-        /* latin-ext */
-        @font-face {
-            font-family: 'Lato';
-            font-style: normal;
-            font-weight: 100;
-            src: local('Lato Hairline'), local('Lato-Hairline'), url({{ URL::asset('assets/fonts/eFRpvGLEW31oiexbYNx7Y_esZW2xOQ-xsNqO47m55DA.woff2') }}) format('woff2');
-            unicode-range: U+0100-024F, U+1E00-1EFF, U+20A0-20AB, U+20AD-20CF, U+2C60-2C7F, U+A720-A7FF;
-        }
-        /* latin */
-        @font-face {
-            font-family: 'Lato';
-            font-style: normal;
-            font-weight: 100;
-            src: local('Lato Hairline'), local('Lato-Hairline'), url({{ URL::asset('assets/fonts/GtRkRNTnri0g82CjKnEB0Q.woff2') }}) format('woff2');
-            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215;
-        }
-    </style>
+        {{--.title {--}}
+            {{--font-size: 96px;--}}
+        {{--}--}}
+        {{--/* latin-ext */--}}
+        {{--@font-face {--}}
+            {{--font-family: 'Lato';--}}
+            {{--font-style: normal;--}}
+            {{--font-weight: 100;--}}
+            {{--src: local('Lato Hairline'), local('Lato-Hairline'), url({{ URL::asset('assets/fonts/eFRpvGLEW31oiexbYNx7Y_esZW2xOQ-xsNqO47m55DA.woff2') }}) format('woff2');--}}
+            {{--unicode-range: U+0100-024F, U+1E00-1EFF, U+20A0-20AB, U+20AD-20CF, U+2C60-2C7F, U+A720-A7FF;--}}
+        {{--}--}}
+        {{--/* latin */--}}
+        {{--@font-face {--}}
+            {{--font-family: 'Lato';--}}
+            {{--font-style: normal;--}}
+            {{--font-weight: 100;--}}
+            {{--src: local('Lato Hairline'), local('Lato-Hairline'), url({{ URL::asset('assets/fonts/GtRkRNTnri0g82CjKnEB0Q.woff2') }}) format('woff2');--}}
+            {{--unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215;--}}
+        {{--}--}}
+    {{--</style>--}}
 </head>
 <body>
-<div class="container">
-    <div class="content">
-        <div class="title">Test</div>
-        <iframe src="http://1782176218.scene.eqh5.cn/s/kBXGDzWQ?eqrcode=1" frameborder="0" style="width: 100%;height: 100%"></iframe>
-    </div>
-</div>
+<iframe src="http://1782176218.scene.eqh5.cn/s/kBXGDzWQ?eqrcode=1" frameborder="0" width="100%" height="100%"></iframe>
+
 </body>
 <script type="text/javascript" charset="UTF-8">
     var mark = Math.random().toString(36).substr(2);
