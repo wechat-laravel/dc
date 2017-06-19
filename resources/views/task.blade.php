b1e8688fdcc84f69418fa6d08401fc351d114f3e
hackqy <hackqy@qq.com>
2017-06-19 10:15:29 +0800

    自定义样式显示测试

--- resources/views/task.blade.php
+++ resources/views/task.blade.php
@@ -22,7 +22,15 @@
     </style>
 </head>
 <body>
-    <iframe src="{{ $task->page_url }}" frameborder="0" width="100%" height="100%"></iframe>
+    @if($task->mark === 'h5')
+        <iframe src="{{ $task->page_url }}" frameborder="0" width="100%" height="100%"></iframe>
+    @else
+        <div class="row">
+            <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
+                {!! $task->editorValue !!}
+            </div>
+        </div>
+    @endif
 </body>
 <script type="text/javascript" charset="UTF-8">
     var mark = Math.random().toString(36).substr(3);
