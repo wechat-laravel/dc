6324059a3d9e0d08412b870bb44008446f6c55f3
hackqy <hackqy@qq.com>
2017-06-23 17:09:57 +0800

    模板地步链接

--- resources/views/task.blade.php
+++ resources/views/task.blade.php
@@ -34,6 +34,9 @@
             </div>
         </div>
     @endif
+    <footer class="main-footer">
+        <strong><a href="http://wewen.io/">一问科技技术支持</a>.</strong>
+    </footer>
 </body>
 <script type="text/javascript" charset="UTF-8">
     $("img").each(function(){$(this).attr("src").indexOf("mmbiz.qpic.cn")>-1&&$(this).attr("src","http://wewen.io/image?src="+$(this).attr("src"))});
