f4a2b3ac2eb89c0e087ff491bfc8db7d427404aa
mr-yuan <810281839@qq.com>
2017-06-23 17:31:54 +0800

    '使用指南'

---
+++ resources/views/modules/admin/help/index.blade.php
@@ -0,0 +1,153 @@
+@extends('modules._layout.admin')
+@section('content')
+    <h2 style="text-align:center">
+        <strong><span style="font-family: 微软雅黑;font-size: 16px"><span style="font-family:微软雅黑">一问信息科技（上海）有限公司数字营销系统使用指南</span></span></strong>
+    </h2>
+    <p>
+        <span style="font-family: 微软雅黑;font-size: 16px"><span style="font-family:微软雅黑">一：登录官网</span> www.maoliduo.cn</span>
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px">&nbsp;</span>
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px"><span style="font-family:微软雅黑">二：点击网站中间文字</span></span>
+    </p>
+    <p>
+        <img src="/upload/images/mmm0.png" style="width:50%">
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px">&nbsp;</span>
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px"><span style="font-family:微软雅黑">三：使用邮箱注册账户</span></span>
+    </p>
+    <p>
+        <img src="/upload/images/mmm1.png" style="width:50%">
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px">&nbsp;</span>
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px"><span style="font-family:微软雅黑">四：当你注册成功之后，是访客模式。暂不能使用我们强大的功能。所以请联系我们的官方客服</span>2905582908，进行开户。</span>
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px">&nbsp;</span>
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px"><span style="font-family:微软雅黑">五：开户成功，你便可以使用功能。</span></span>
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px">&nbsp;</span>
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px"><span style="font-family:微软雅黑">六：编辑文章。</span></span>
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px">1，封装链接，输入一篇文章的url便可以进行导入文章。</span>
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px">2，图文原创，自行编辑。</span>
+    </p>
+    <p>
+        <img src="/upload/images/mmm2.png" style="width:100%">
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px">&nbsp;</span>
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px"><span style="font-family:微软雅黑">七：点击你的任务列表，点击查看</span></span>
+    </p>
+    <p>
+        <img src="/upload/images/mmm3.png" style="width:100%">
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px"><span style="font-family:微软雅黑">使用手机扫描二维码，便可以进行阅览文章，和进行分享转发传播了。</span></span>
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px">&nbsp;</span>
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px"><span style="font-family:微软雅黑">八：当文章开始传播，你的数据便被记录下来。</span></span>
+    </p>
+    <p>
+        <img src="/upload/images/mmm4.png" style="width:50%">
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px"><span style="font-family:微软雅黑">点击数据图，便可以看到你的数据传播效果。如下：</span></span>
+    </p>
+    <p>
+        <img src="/upload/images/mmm5.png" style="width:100%">
+    </p>
+    <p>
+        <img src="/upload/images/mmm6.png" style="width:100%">
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px">&nbsp;</span>
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px"><span style="font-family:微软雅黑">点击关系图。效果如下</span></span>
+    </p>
+    <p>
+        <img src="/upload/images/mmm7.png" style="width:100%">
+    </p>
+    <p>
+        <img src="/upload/images/mmm8.png" style="width:100%">
+    </p>
+    <p>
+        <img src="/upload/images/mmm9.png" style="width:100%">
+    </p>
+    <p>
+        <img src="/upload/images/mmm10.png" style="width:100%">
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px">&nbsp;</span>
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px"><span style="font-family:微软雅黑">九：服务功能。</span></span>
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px"><span style="font-family:微软雅黑">为了提高打造高质量的营销。我们在传播的途径中加入了红包功能。</span></span>
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px"><span style="font-family:微软雅黑">红包工具使用方法</span></span>
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px">1，打开左侧栏目红包工具，进入之后，点击添加配置</span>
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px">&nbsp;</span>
+    </p>
+    <p>
+        <img src="/upload/images/mmm11.png" style="width:100%">
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px">2，</span>
+    </p>
+    <p>
+        <img src="/upload/images/mmm12.png" style="width:50%">
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px">&nbsp;</span>
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px">3，当你提交成功之后，你的文章在传播途中便会自动给满足条件的用户发放红包。那么就可以感受指数般的数据传播了！</span>
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px">&nbsp;</span>
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px"><span style="font-family:微软雅黑">注：红包工具需要你的账户上有充足的余额。余额充值，请联系</span>qq：2905582908</span>
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px"><span style="font-family:微软雅黑">一问信息科技（上海）有限公司</span></span>
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px"><span style="font-family:微软雅黑">地址：上海市普陀区长寿路</span>1076号飞雕商务大厦1505</span>
+    </p>
+    <p>
+        <span style=";font-family:微软雅黑;font-size:16px"><span style="font-family:微软雅黑">电话：</span>021-36213161</span>
+    </p>
+    <p>
+        <br/>
+    </p>
+@endsection
