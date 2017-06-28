96d4dce501e13e74d961591526b92bc25aed7a30
hackqy <hackqy@qq.com>
2017-06-28 17:26:45 +0800

    名称长度不能超过10

--- public/assets/js/admin/service/red_bag.js
+++ public/assets/js/admin/service/red_bag.js
@@ -509,8 +509,8 @@ $('#editConfigModal').bootstrapValidator({
                 },
                 stringLength: {
                     min: 1,
-                    max: 30,
-                    message: '红包发送者名称最大为30个字符'
+                    max: 10,
+                    message: '红包发送者名称最大为10个字符'
                 }
             }
         },
