5bea65647b3fbd0566b28686af76b3b0156ebaf0
hackqy <hackqy@qq.com>
2017-07-27 17:40:08 +0800

    算法优化

--- app/Http/Controllers/Admin/Data/WechatController.php
+++ app/Http/Controllers/Admin/Data/WechatController.php
@@ -22,21 +22,18 @@ class WechatController extends Controller
         $id = intval($id);
 
         if (Auth::user()->identity !== 'admin'){
-
-            $task = TasksModel::where('user_id',Auth::id())->where('id',$id)->first();
+            //用哪些列取哪些列，不要全部取出来
+            $task = TasksModel::select('id','title')->where('user_id',Auth::id())->where('id',$id)->first();
 
         }else{
 
-            $task = TasksModel::find($id);
+            $task = TasksModel::select('id','title')->where('id',$id)->first();
 
         }
 
         if (!$task) return response()->json(['success'=>false,'msg'=>'非法的请求！']);
 
-
         if ($request->ajax()) {
-
-//            $avg = $this->avg($id);
 
             //数据统计
             $top = [
@@ -74,7 +71,7 @@ class WechatController extends Controller
                     ['value'=>0,'name'=>'QQ空间'],                       //分享统计
                 ],
                 'current'=>[                                             //当日的每小时走势
-                    'day'   => [],
+                    'day'   => [],                                       //当日整点的时间字符串（如：07-20 0:00..）
                     'pv'    => [
                         0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
                     ],
@@ -85,7 +82,7 @@ class WechatController extends Controller
                         0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
                     ],
                 ],
-                'visit'=>[                                               //访问时间分布
+                'visit'=>[                                               //访问时间分布的每小时走势
                     'this'=>[
                         0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
                     ],
@@ -104,57 +101,75 @@ class WechatController extends Controller
             }
 
             //最多统计7天
-
             $today = date('Y-m-d', time());
 
             $today = strtotime($today);
-
+            //7天的时间戳（如：1501147718）
             $days = [$today, $today - 86400, $today - 172800, $today - 259200, $today - 345600, $today - 432000, $today - 518400];
-
+            //7天的时间（如：7-27）
             $top['days'] = [date('m-d', $days[6]), date('m-d', $days[5]), date('m-d', $days[4]), date('m-d', $days[3]), date('m-d', $days[2]), date('m-d', $days[1]), date('m-d', $days[0])];
 
             $uv = [[], [], [], [], [], [], [], []];
 
-            //总浏览量
-            $res = SpreadRecordModel::select('openid','source','action', 'stay', 'level', 'created_at')->where('tasks_id',$id)->orderBy('created_at', 'asc')->get();
+            //因为不能一次性把数据都取出来，先分批处理。 先查出多少条数据，然后每次1000条的取
+            $times = SpreadRecordModel::where('tasks_id',$id)->count('id');
+
+            $times = ceil($times/1000);
+
+            $res = [];
+
+            for ($i =0 ; $i <= $times;$i++){
+                //偏移量
+                $offset = $i * 1000;
+                //每次取出的条数
+                $limit  = 1000;
+
+                if (empty($res)){
+
+                    $res = SpreadRecordModel::select('openid','source','action', 'stay', 'level', 'created_at')->where('tasks_id',$id)->orderBy('created_at', 'asc')->skip($offset)->take($limit)->get()->toArray();
+
+                }else{
+
+                    $res = array_merge($res,SpreadRecordModel::select('openid','source','action', 'stay', 'level', 'created_at')->where('tasks_id',$id)->orderBy('created_at', 'asc')->skip($offset)->take($limit)->get()->toArray());
+
+                }
+
+            }
 
             foreach ($res as $re) {
-
-                //var_dump();exit;
-
                 //PV,UV,分享统计与走势
-                if ($re->action === 'browse') {
+                if ($re['action'] === 'browse') {
 
                     //今天
-                    if (strtotime($re->created_at) >= $days[0]) {
+                    if ($re['created_at'] >= $days[0]) {
 
                         $top['pv_today'] += 1;
 
-                        $top['current']['pv'][intval(substr($re->created_at,11,2))] += 1;
+                        $top['current']['pv'][intval(date('H',$re['created_at']))] += 1;
 
                         $uv[0] = [];
 
-                        if (!in_array($re->openid, $uv[6])) {
+                        if (!in_array($re['openid'], $uv[6])) {
 
-                            $uv[6][] = $re->openid;
+                            $uv[6][] = $re['openid'];
 
                             $top['uv_today'] += 1;
 
                             $top['uv_everyday'][6] += 1;
 
-                            $top['current']['uv'][intval(substr($re->created_at,11,2))] += 1;
+                            $top['current']['uv'][intval(date('H',$re['created_at']))] += 1;
 
                         }
 
                         $top['pv_everyday'][6] += 1;
 
-                    } elseif (strtotime($re->created_at) >= $days[1] && strtotime($re->created_at) < $days[0]) {
+                    } elseif ($re['created_at'] >= $days[1] && $re['created_at'] < $days[0]) {
 
                         $top['pv_yesterday'] += 1;
 
-                        if (!in_array($re->openid, $uv[5])) {
+                        if (!in_array($re['openid'], $uv[5])) {
 
-                            $uv[5][] = $re->openid;
+                            $uv[5][] = $re['openid'];
 
                             $top['uv_yesterday'] += 1;
 
@@ -164,11 +179,11 @@ class WechatController extends Controller
 
                         $top['pv_everyday'][5] += 1;
 
-                    } elseif (strtotime($re->created_at) >= $days[2] && strtotime($re->created_at) < $days[1]) {
+                    } elseif ($re['created_at'] >= $days[2] && $re['created_at'] < $days[1]) {
 
-                        if (!in_array($re->openid, $uv[4])) {
+                        if (!in_array($re['openid'], $uv[4])) {
 
-                            $uv[4][] = $re->openid;
+                            $uv[4][] = $re['openid'];
 
                             $top['uv_everyday'][4] += 1;
 
@@ -176,11 +191,11 @@ class WechatController extends Controller
 
                         $top['pv_everyday'][4] += 1;
 
-                    } elseif (strtotime($re->created_at) >= $days[3] && strtotime($re->created_at) < $days[2]) {
+                    } elseif ($re['created_at'] >= $days[3] && $re['created_at'] < $days[2]) {
 
-                        if (!in_array($re->openid, $uv[3])) {
+                        if (!in_array($re['openid'], $uv[3])) {
 
-                            $uv[3][] = $re->openid;
+                            $uv[3][] = $re['openid'];
 
                             $top['uv_everyday'][3] += 1;
 
@@ -188,11 +203,11 @@ class WechatController extends Controller
 
                         $top['pv_everyday'][3] += 1;
 
-                    } elseif (strtotime($re->created_at) >= $days[4] && strtotime($re->created_at) < $days[3]) {
+                    } elseif ($re['created_at'] >= $days[4] && $re['created_at'] < $days[3]) {
 
-                        if (!in_array($re->openid, $uv[2])) {
+                        if (!in_array($re['openid'], $uv[2])) {
 
-                            $uv[2][] = $re->openid;
+                            $uv[2][] = $re['openid'];
 
                             $top['uv_everyday'][2] += 1;
 
@@ -200,11 +215,11 @@ class WechatController extends Controller
 
                         $top['pv_everyday'][2] += 1;
 
-                    } elseif (strtotime($re->created_at) >= $days[5] && strtotime($re->created_at) < $days[4]) {
+                    } elseif ($re['created_at'] >= $days[5] && $re['created_at'] < $days[4]) {
 
-                        if (!in_array($re->openid, $uv[1])) {
+                        if (!in_array($re['openid'], $uv[1])) {
 
-                            $uv[1][] = $re->openid;
+                            $uv[1][] = $re['openid'];
 
                             $top['uv_everyday'][1] += 1;
 
@@ -212,11 +227,11 @@ class WechatController extends Controller
 
                         $top['pv_everyday'][1] += 1;
 
-                    } elseif (strtotime($re->created_at) >= $days[6] && strtotime($re->created_at) < $days[5]) {
+                    } elseif ($re['created_at'] >= $days[6] && $re['created_at'] < $days[5]) {
 
-                        if (!in_array($re->openid, $uv[0])) {
+                        if (!in_array($re['openid'], $uv[0])) {
 
-                            $uv[0][] = $re->openid;
+                            $uv[0][] = $re['openid'];
 
                             $top['uv_everyday'][0] += 1;
 
@@ -226,67 +241,67 @@ class WechatController extends Controller
 
                     }
 
-                    if (!in_array($re->openid, $uv[7])) {
+                    if (!in_array($re['openid'], $uv[7])) {
 
-                        $uv[7][] = $re->openid;
+                        $uv[7][] = $re['openid'];
 
                         $top['uv_num'] += 1;
 
-                        $top['level']['uv'][$re->level] += 1;
+                        $top['level']['uv'][$re['level']] += 1;
 
                     }
 
                     $top['pv_num'] += 1;
 
-                    $top['level']['pv'][$re->level] += 1;
+                    $top['level']['pv'][$re['level']] += 1;
 
-                    $top['stay_avg'] += $re->stay;
+                    $top['stay_avg'] += $re['stay'];
 
                     //时间段统计
-                    if ($re->stay <= 5){
+                    if ($re['stay'] <= 5){
 
                         $top['stay']['this'][0] += 1;
 
-                    }elseif ($re->stay > 5 && $re->stay <= 10 ){
+                    }elseif ($re['stay'] > 5 && $re['stay'] <= 10 ){
 
                         $top['stay']['this'][1] += 1;
 
-                    }elseif ($re->stay > 10 && $re->stay <= 20 ){
+                    }elseif ($re['stay'] > 10 && $re['stay'] <= 20 ){
 
                         $top['stay']['this'][2] += 1;
 
-                    }elseif ($re->stay > 20 && $re->stay <= 40 ){
+                    }elseif ($re['stay'] > 20 && $re['stay'] <= 40 ){
 
                         $top['stay']['this'][3] += 1;
 
-                    }elseif ($re->stay > 40 && $re->stay <= 80 ){
+                    }elseif ($re['stay'] > 40 && $re['stay'] <= 80 ){
 
                         $top['stay']['this'][4] += 1;
 
-                    }elseif ($re->stay > 80 && $re->stay <= 160 ){
+                    }elseif ($re['stay'] > 80 && $re['stay'] <= 160 ){
 
                         $top['stay']['this'][5] += 1;
 
-                    }elseif ($re->stay > 160 && $re->stay <= 320 ){
+                    }elseif ($re['stay'] > 160 && $re['stay'] <= 320 ){
 
                         $top['stay']['this'][6] += 1;
 
-                    }elseif ($re->stay > 320 && $re->stay <= 640 ){
+                    }elseif ($re['stay'] > 320 && $re['stay'] <= 640 ){
 
                         $top['stay']['this'][7] += 1;
 
-                    }elseif ($re->stay > 640 && $re->stay <= 1280 ){
+                    }elseif ($re['stay'] > 640 && $re['stay'] <= 1280 ){
 
                         $top['stay']['this'][8] += 1;
 
-                    }elseif ($re->stay > 1280 ){
+                    }elseif ($re['stay'] > 1280 ){
 
                         $top['stay']['this'][9] += 1;
 
                     }
 
                     //统计来源数据
-                    switch ($re->source){
+                    switch ($re['source']){
 
                         //单人对话
                         case 'wechat':
@@ -323,42 +338,42 @@ class WechatController extends Controller
                             break;
                     }
 
-                    $top['visit']['this'][intval(substr($re->created_at,11,2))] += 1;
+                    $top['visit']['this'][intval(date('H',$re['created_at']))] += 1;
 
                 } else {
 
                     //今天
-                    if (strtotime($re->created_at) >= $days[0]) {
+                    if ($re['created_at'] >= $days[0]) {
 
                         $top['share_today'] += 1;
 
                         $top['share_everyday'][6] += 1;
 
-                        $top['current']['share'][intval(substr($re->created_at,11,2))] += 1;
+                        $top['current']['share'][intval(date('H',$re['created_at']))] += 1;
 
-                    } elseif (strtotime($re->created_at) >= $days[1] && strtotime($re->created_at) < $days[0]) {
+                    } elseif ($re['created_at'] >= $days[1] && $re['created_at'] < $days[0]) {
 
                         $top['share_yesterday'] += 1;
 
                         $top['share_everyday'][5] += 1;
 
-                    } elseif (strtotime($re->created_at) >= $days[2] && strtotime($re->created_at) < $days[1]) {
+                    } elseif ($re['created_at'] >= $days[2] && $re['created_at'] < $days[1]) {
 
                         $top['share_everyday'][4] += 1;
 
-                    } elseif (strtotime($re->created_at) >= $days[3] && strtotime($re->created_at) < $days[2]) {
+                    } elseif ($re['created_at'] >= $days[3] && $re['created_at'] < $days[2]) {
 
                         $top['share_everyday'][3] += 1;
 
-                    } elseif (strtotime($re->created_at) >= $days[4] && strtotime($re->created_at) < $days[3]) {
+                    } elseif ($re['created_at'] >= $days[4] && $re['created_at'] < $days[3]) {
 
                         $top['share_everyday'][2] += 1;
 
-                    } elseif (strtotime($re->created_at) >= $days[5] && strtotime($re->created_at) < $days[4]) {
+                    } elseif ($re['created_at'] >= $days[5] && $re['created_at'] < $days[4]) {
 
                         $top['share_everyday'][1] += 1;
 
-                    } elseif (strtotime($re->created_at) >= $days[6] && strtotime($re->created_at) < $days[5]) {
+                    } elseif ($re['created_at'] >= $days[6] && $re['created_at'] < $days[5]) {
 
                         $top['share_everyday'][0] += 1;
 
@@ -366,10 +381,10 @@ class WechatController extends Controller
 
                     $top['share_num'] += 1;
 
-                    $top['level']['share'][$re->level] += 1;
+                    $top['level']['share'][$re['level']] += 1;
 
                     //统计分享去向
-                    switch ($re->action){
+                    switch ($re['action']){
 
                         //QQ好友
                         case 'qq':
@@ -442,7 +457,6 @@ class WechatController extends Controller
             }
 
             //计算访问时间的各时间段的占比
-
             for ($i =0 ;$i <24; $i++){
 
                 if ($top['pv_num'] === 0){
