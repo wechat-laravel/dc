69caa26a573ad7e832d714d838d7924738468147
hackqy <hackqy@qq.com>
2017-08-03 16:36:22 +0800

    添加图标

--- resources/views/modules/_layout/admin.blade.php
+++ resources/views/modules/_layout/admin.blade.php
@@ -184,9 +184,9 @@
                         </span>
                     </a>
                     <ul class="treeview-menu">
-                        <li :class="{active: @three==='service_red_bag'}"><a href="/admin/service/red_bag"><i class="fa fa-circle-o"></i>红包工具</a></li>
-                        <li :class="{active: @three==='service_ad_column'}"><a href="/admin/service/ad_column"><i class="fa fa-circle-o"></i>广告栏设置</a></li>
-                        <li :class="{active: @three==='service_mass'}"><a href="/admin/service/mass"><i class="fa fa-circle-o"></i>群发助手</a></li>
+                        <li :class="{active: @three==='service_red_bag'}"><a href="/admin/service/red_bag"><i class="fa fa-hacker-news"></i>红包工具</a></li>
+                        <li :class="{active: @three==='service_ad_column'}"><a href="/admin/service/ad_column"><i class="fa fa-file-audio-o"></i>广告栏设置</a></li>
+                        <li :class="{active: @three==='service_mass'}"><a href="/admin/service/mass"><i class="fa fa-send-o"></i>群发助手</a></li>
                     </ul>
                 </li>
                 @endif
